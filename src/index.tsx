8009392d1653dc68dcfd9e7d2642159db712fb22
Атаманив Владимир Петрович <uk30640@npp>
2018-07-24 16:45:06 +0300

    Signed-off-by: Атаманив Владимир Петрович

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,34 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom'; 
-import {observable, action} from 'mobx';
+import {observable, action, computed} from 'mobx';
 import {observer} from 'mobx-react';
 import DevTools from 'mobx-react-devtools';
 
 class AppState {
-  @observable Deep = 0;
+  @observable a = 1;
+  @observable b = 1;
+
   @observable Show = 'visible';
   constructor() {
+     
   }
-  @action onclick = () => {
-    this.Deep++;
+  @action onclickA = () => {
+    this.a++;
+  }
+  @action onclickB = () => {
+    this.b++;
   }
   @action showMy = () =>{
     if (this.Show === 'visible' ) 
       this.Show = 'hidden' 
     else 
         this.Show = 'visible';
+  }
+  @computed get perimetr() {
+    return (this.a+this.b)*2;
+  }
+  perim () {
+    return (this.a+this.b)*2;
   }
 }
 
@@ -30,13 +42,17 @@ render() {
             <div style={{justifyContent: 'center', background:'#ddd', border :'4px double black',textAlign: 'center', flexWrap: 'wrap', flexDirection: 'column', width:200}}>
               <DevTools />
             <div style={{textAlign: 'center', padding: 25, visibility:appState.Show, background:'#eee'}}> 
-               <p>Hello react mobx typescrypt &nbsp; {appState.Deep}</p>            
+               <p>Hello react mobx typescrypt</p>
+               <p>width (a): {appState.a} &nbsp; <br/>height (b): {appState.b} </p>
             </div>  
             <div style={{ padding:10}}>
-               <button onClick={appState.onclick}>Increment</button>
+               <button onClick={appState.onclickA}>Increment A</button>
+               <button onClick={appState.onclickB}>Increment B</button>
             </div>
             <div style={{ padding:10}}>
               <button onClick={appState.showMy}>{appState.Show}</button>
+              <p>Perimetr computed {appState.perimetr}</p>
+              <p>Perimetr not computed {appState.perim()}</p>
             </div>  
             </div>
           </div>);
